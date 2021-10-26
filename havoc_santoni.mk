d83fb9806fad0c2d5a6fd0b544e92b353825cb0c
sukeeratsg <sukeerat@goindi.org>
2021-10-26 22:32:45 +0530

    Havoc : Add wallpaper types and clean up spelling mistakes

--- havoc_santoni.mk
+++ havoc_santoni.mk
@@ -1,6 +1,13 @@
 # Inherit makefiles
+
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 $(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
-# ROM spesific makefile name
+# ROM specific makefiles
+
 PRODUCT_NAME := havoc_santoni
+
+#  Custom  Flags
+
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
